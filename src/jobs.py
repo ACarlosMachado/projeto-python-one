8e64085fce1cb9b32b2f744c6cd4cc16222abf59
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-11-07 20:25:39 -0300

    refactor: requisito 1

--- src/jobs.py
+++ src/jobs.py
@@ -5,19 +5,11 @@ import csv
 @lru_cache
 def read(path):
     with open(path, "r") as file:
-        jobs = csv.reader(file, delimiter=",", quotechar='"')
-        header, *datas = jobs
+        jobs = csv.DictReader(file, delimiter=",", quotechar='"')
         result = []
-        linhas = ({heade: "" for heade in header})
-        for i, heade in enumerate(header):
-            for f, data in enumerate(datas):
-                if i == f:
-                    linhas.update({heade: data[f]})
-                    result.append(linhas)
-                else:
-                    ...
-    print(f"Aqui === {len(result)}")
-    return result[0:13]
+        for job in jobs:
+            result.append(job)
+        return result
 
 
 read("./src/jobs.csv")
