30e8e6fef54a1d62a6ea0f5e86267eb743620b64
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-11-09 20:18:43 -0300

    test: function test_brazilian_jobs()

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    get_jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    key_in_english = {'title': '', 'salary': 0, 'type': 0}
+    for job in get_jobs:
+        assert job.keys() == key_in_english.keys()
