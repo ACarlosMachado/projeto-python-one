2ba0a9c41bd4fbe6b539d2782f7835b3117c1805
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-11-11 15:11:50 -0300

    refactor: flask route /job/<index>

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -76,7 +76,7 @@ def list_jobs():
 
 
 @bp.route("/job/<index>")
-def get_job_by_index(index):
+def job(index):
     jobs = read('./src/jobs.csv')
     job = get_job(jobs, index)
     return render_template("job.jinja2", job=job)
